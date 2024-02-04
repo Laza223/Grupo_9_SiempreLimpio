15f4d64c492585ba005299b996164dd15beb5935
jose10130 <jose10130.jcd@gmail.com>
2024-02-04 04:15:38 -0300

    haciendo el login hasta q este la base

--- app.js
+++ app.js
@@ -5,4 +5,11 @@ const path=require("path")
 const pórt= 3030;
 app.get(`/`,(req,res) => {res.sendFile(path.join(__dirname,`./views/home.html`))})
 
+app.get('/login', (req, res)=>{
+    res.sendFile(path.join(__dirname, "./views/login.html"));
+
+app.get('/login', (req, res)=>{
+    res.redirect('/home')
+})
+})
 app.listen(port ,()=>console.log(`http://localhost: ${port}`))
