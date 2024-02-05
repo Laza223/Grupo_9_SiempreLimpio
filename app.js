061d0086888391f1f99b34af7a799876739e2a7b
Rodrigo Ezequiel Ruiz <98621578+Rakasei@users.noreply.github.com>
2024-02-05 10:14:32 -0300

    App.js actualización

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const express = require(" express");
-const app= express();
-const path=require("path")
+/* Puerto para el servidor */
+const port = 3030;
 
-const pórt= 3030;
-app.get(`/`,(req,res) => {res.sendFile(path.join(__dirname,`./views/home.html`))})
 
-app.listen(port ,()=>console.log(`http://localhost: ${port}`))
+const express = require('express');
+
+const app = express()
+
+const path = require('path')
+
+app.use(express.static('public'))
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, "./views/home.html"))
+})
+
+app.get('/home', (req, res)=>{
+    res.redirect('/')
+})
+
+
+
+app.listen(port, ()=> console.log(`http://localhost:${port}`))
