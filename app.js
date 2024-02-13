26d8b0a5b86a7f2eb02d11dcf5ce17075a6c8a14
Laza223 <lazarofeijoo2004@gmail.com>
2024-02-13 14:04:46 -0300

    Arreglando conflictos

--- app.js
+++ app.js
@@ -2,17 +2,15 @@ const express = require("express")
 
 const path = require("path")
 
-<<<<<<< HEAD
-const express = require('express');
-=======
 const port = 3030
->>>>>>> Laza
 
 const app = express()
 
 app.use(express.static("public"))
 
-app.listen(port, () => console.log("http://localhost:3030/"))
+
+app.get("/home", (req, res) => { res.redirect("/") }
+)
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/index.html"))
@@ -22,11 +20,4 @@ app.get("/producto", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/detalleProducto.html"))
 })
 
-<<<<<<< HEAD
-
-
-app.listen(port, ()=> console.log(`http://localhost:${port}`))
-=======
-app.get("/home", (req, res) => { res.redirect("/") }
-)
->>>>>>> Laza
+app.listen(port, () => console.log("http://localhost:3030/"))
