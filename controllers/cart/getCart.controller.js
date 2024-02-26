77adcb5a430f6d2b25453efbeca1e92025845d63
Rodrigo Ezequiel Ruiz <98621578+Rakasei@users.noreply.github.com>
2024-02-25 21:37:01 -0300

    Subiendo cambios locales de la rama sprint3-rodrigo

--- controllers/cart/getCart.controller.js
+++
@@ -1,7 +0,0 @@
-const createLog = require("../../utils/createLog");
-module.exports = (req, res) => {
-  res.render("cart")
-
-  const time = new Date().toTimeString()
-  createLog("El usuario accedio al CARRITO DE COMPRAS a las " + time)
-}
