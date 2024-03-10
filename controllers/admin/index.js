f635f5e1b67af005e57e26a423e7e9e8de561e50
jose10130 <jose10130.jcd@gmail.com>
2024-03-10 01:51:03 -0300

    agrege delete

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -2,6 +2,7 @@ const updateController = require("./update.controller");
 module.exports = {
     home: (req,res) => res.render("./dashboard"),
     create: require("./create.controller"),
-  update: updateController.detail
+    update: updateController.detail,
+    deleteProduct: require("./deleteProduct.controller.js"),
   };
   
