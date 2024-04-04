4fcda47083249aa6aa65d1b035303f3cebe90515
Rodrigo Ezequiel Ruiz <98621578+Rakasei@users.noreply.github.com>
2024-04-04 19:33:08 -0300

    Agrego validaciones

--- src/routes/authentication.routes.js
+++ src/routes/authentication.routes.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const { login, procesoIniciar, logout, register, registerProcess} = require("../controllers/authentication");
 
+const { login, procesoIniciar, logout, register, registerProcess} = require("../controllers/authentication");
+const { validation } = require("../middlewares/validations");
+
+
+//Falta agregar validaciones
 
 // Login
 router.get("/iniciar", login); 
 router.post("/iniciar", procesoIniciar); 
 
-// Registro
+// Registro  
+
 router.get("/registrar", register); 
-router.post("/registrar", registerProcess)
+router.post("/registrar", registerProcess);
+
+
 
 //Cierre de sesión
 router.get("/cerrar-sesion", logout)
