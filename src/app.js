ad00d52cf484065c2e08cfde68691acf6abce975
Lukemciel <lucasmaciel7ynine@gmail.com>
2024-04-18 23:28:10 -0300

    Merge rama LM con develop

--- src/app.js
+++ src/app.js
@@ -61,16 +61,17 @@ app.use((req,res, next) => {
 }); */
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development}
-  console.log("error",err);
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+
+//   app.use(function(err, req, res, next) {
+//     // set locals, only providing error in development}
+//     console.log("error",err);
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+//     
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+//   });
 
 app.use(methodOverride('_method')); //PUT Y DELETE HABILITADO
 
