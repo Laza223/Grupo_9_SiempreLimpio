df3037bacfd7661c2b1f856d253744407f56d90a
Lukemciel <lucasmaciel7ynine@gmail.com>
2024-04-19 02:55:04 -0300

    estilos mejorados para home

--- src/app.js
+++ src/app.js
@@ -28,6 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(partials());
 
 app.use(session({ secret: "palabra secreta"}) );
 app.use(checkUser)
