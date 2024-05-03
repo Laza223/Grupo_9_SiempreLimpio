d48a297b0879fa7e3c3e5f00647c9993d79242fe
Laza223 <Lazarofeijoo2004@gmail.com>
2024-05-03 19:00:00 -0300

    Subiendo modelos

---
+++ src/db/migrations/20240503215421-create-order.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_Id: {
+        type: Sequelize.INTEGER
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      status_id: {
+        type: Sequelize.INTEGER
+      },
+      shipping_address: {
+        type: Sequelize.STRING
+      },
+      payment_method: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Orders');
+  }
+};
