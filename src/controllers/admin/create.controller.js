3417d2b0bac0dd15883244c76ddde97a5521e097
tamara-diaz <tamydiaz.itf@gmail.com>
2024-05-06 14:26:08 -0300

    implementando refactorizacion de crear producto

--- src/controllers/admin/create.controller.js
+++ src/controllers/admin/create.controller.js
@@ -1,4 +1,11 @@
-module.exports = (req, res) => {
-    res.render("admin/createProduct")
-}
+const db=require("../../database/models")
 
+module.exports = (req, res) => {
+    db.categoria.findAll()
+    .then((categoria) => {
+      res.render("admin/createProduct",{ categoria },(err, contentView) => {
+        err && res.send(err.message)
+        res.render("partials/dashboard", { contentView })
+      })
+    })
+  };
--- src/controllers/admin/storeProduct.controller.js
+++ src/controllers/admin/storeProduct.controller.js
@@ -1,17 +1,39 @@
 const path = require("path")
-
 let products = require("../../database/products.json");
 const fs = require("fs");
+const {validationResult}=require("express-validator");
+
+const db=require("../../db/models")
 
 module.exports = (req, res) => {
+const errors=validationResult(req);
+if (errors.isEmpty()){
+const { nombre, precio, descripcion, categoria, disponible,stock } = req.body;
 
-  const { nombre, precio, descripcion, categoria, disponible,stock } = req.body;
+  db.product.create({
+    nombre:nombre.trim(),
+    precio: +precio,
+    descripcion:descripcion.trim(),
+    imagen:req.files.imagen?.length ? req.files.imagen[0]?.filename:
+    "not-image.png",
+    categoriaId:+categoria,
+    stock: +stock,
+    disponible:disponible==="on"
 
-  
-  const newId = products[products.length - 1].id + 1;
+  })
+  .then((product)=>{
+    return res.redirect("/admin/product")
 
-  
-  const newProduct = {
+  })
+  .catch(err => {
+    res.send(err.message)
+})
+
+  }
+  return res.redirect("/admin/product")
+};
+  /*const newId = products[products.length - 1].id + 1;*/
+  /* const newProduct = {
     id: newId,
     nombre:nombre.trim(),
     precio: +precio,
@@ -21,14 +43,8 @@ module.exports = (req, res) => {
     stock: +stock,
     disponible:disponible==="on"
   };
-
-
-
   products = [...products, newProduct];
 
   products = JSON.stringify(products, null, 3);
   const pathProducts = path.join(__dirname, "../../database/products.json");
-  fs.writeFileSync(pathProducts, products, "utf-8");
-
-  return res.redirect("/admin/product")
-};
+  fs.writeFileSync(pathProducts, products, "utf-8")*/
