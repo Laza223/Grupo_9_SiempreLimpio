8a001c3c59d8fe62171f1d590d0ed2ffa6f6eafe
Laza223 <Lazarofeijoo2004@gmail.com>
2024-05-10 04:22:40 -0300

    Arreglando error de boton header iniciar

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
+    "password": "2323",
     "database": "siempre_limpio_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
